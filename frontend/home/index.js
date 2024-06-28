8fa5a2f3dd175fb16d1e458689123eca79f3c26c
manurajtm7 <manurajtm707@gmail.com>
2024-06-28 14:41:07 +0530

    home page changes

--- frontend/home/index.js
+++ frontend/home/index.js
@@ -3,6 +3,7 @@ console.log("hello from script...");
 let toggleMode = false;
 const toggleList = document.getElementById("toggle-list");
 const toggleIcon = document.getElementById("toggle-icon");
+const settingsIcon = document.getElementById("setting-icon");
 
 //mode requires
 let lightMode = document.getElementById("light-mode");
@@ -13,23 +14,27 @@ let presentationContainer = document.getElementById("presentation-container");
 const changeToggleMode = () => {
   console.log(toggleMode);
   toggleList.classList.toggle("hidden");
-  toggleMode = !toggleMode;
-  //   if (!toggleMode) toggleList.style.display = "hidden";
 };
 
-
-lightMode.onclick = function(){
-    MainContainer.style.color = "black"
-    MainContainer.style.backgroundColor = "white"
-    MainContainer.classList.remove("bg-zinc-900")
-    presentationContainer.style.backgroundColor = "whiite"
-}
-darkMode.onclick = function(){
-    MainContainer.style.color = "white"
-    MainContainer.classList.add("bg-zinc-900")
-    MainContainer.style.backgroundColor = "black"
-    presentationContainer.style.backgroundColor  = "black"
-}
-
-
 toggleIcon.addEventListener("click", () => changeToggleMode());
+
+//? mode change functions
+
+lightMode.onclick = function () {
+  toggleList.style.color = "black"
+  MainContainer.style.color = "black";
+  MainContainer.style.backgroundColor = "white";
+  MainContainer.classList.remove("bg-zinc-900");
+  presentationContainer.style.backgroundColor = "white";
+  settingsIcon.src = "../assests/gear.png";
+  console.log("dark mode to white");
+};
+darkMode.onclick = function () {
+  MainContainer.style.color = "white";
+  MainContainer.classList.add("bg-zinc-900");
+  MainContainer.style.backgroundColor = "black";
+  presentationContainer.style.backgroundColor = "black";
+  settingsIcon.src = "../assests/gear-white.png";
+  console.log("white mode to dark");
+};
+
