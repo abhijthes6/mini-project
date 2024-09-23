04c202b35037bd9178cdb4ce00b9d29bd3fb3f61
abhijithes6 <abhijithes266@gmail.com>
2024-09-23 12:32:48 +0530

    connection

--- backend/connection/php-config/connection-php.php
+++ backend/connection/php-config/connection-php.php
@@ -1,2 +1,8 @@
-<?
-echo "test";
+<?php
+echo "Service started  successfully...!";
+$conn = new mysqli("localhost", "root","","hire_hub");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+  ?>
